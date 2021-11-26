6a89b28b4e577e6dc2c21c8118fa4b9f33796c56
Tenysson <tenyssonwill@gmail.com>
2021-11-26 19:03:38 -0300

    First Commit

---
+++ app/src/main/java/com/example/tenysson/lixeiracoletiva/CadastrarColeta.java
@@ -0,0 +1,13 @@
+package com.example.tenysson.lixeiracoletiva;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastrarColeta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_coleta);
+    }
+}
